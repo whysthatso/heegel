723b9595099327a91defa1f994ec601c36352e28
Andreas Wagner <whysthatso@gmx.de>
2013-04-10 02:05:37 +0300

    gemfile to http

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,7 @@ set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
+ssh_options[:keys] = %w('~/.ssh/vs01-nc-keys/vs01-nc-heegel')
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
@@ -29,14 +30,14 @@ namespace :deploy do
     sudo "ln -nfs #{current_path}/config/heegel /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
-    #put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
     puts "Now edit the config files in #{shared_path}."
   end
   after "deploy:setup", "deploy:setup_config"
 
-  #task :symlink_config, roles: :app do
-  #  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  #end
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
   after "deploy:finalize_update", "deploy:symlink_config"
 
   desc "Make sure local git is in sync with remote."
